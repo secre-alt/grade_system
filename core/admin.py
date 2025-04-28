cd08a91f1be14e10f2a5f7d148af4f4be652126e
secre-alt <triannesano@gmail.com>
2025-04-27 19:43:06 -0700

    up

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import CustomUser, Grade, StudentProfile, TeacherProfile
+
 # Register your models here.
+admin.site.site_header = "Grade System Project"
+admin.site.site_title = "Grade System"
+
 
 admin.site.register(CustomUser)
 admin.site.register(Grade)
